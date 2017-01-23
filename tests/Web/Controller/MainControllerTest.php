454e3c0da48ed70f60054bf28f1702061f1cd579
kmonmousseau <k.monmousseau@gmail.com>
2017-01-23 21:04:49 +0100

    fix web test case

--- tests/Web/Controller/MainControllerTest.php
+++ tests/Web/Controller/MainControllerTest.php
@@ -4,14 +4,22 @@ namespace Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class MainControllerTest extends WebTestCase
 {
     public function testIndex()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
 
+        $crawler = $client->request('GET', '/en/');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $crawler = $client->request('GET', '/kr/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $crawler = $client->request('GET', '/fr/');
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
     }
 }
